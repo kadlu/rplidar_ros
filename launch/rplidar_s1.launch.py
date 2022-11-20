279807b244b69fe55e9e83c86ad817a5656126f2
kadlu <50589471+kadlu@users.noreply.github.com>
2022-11-20 17:29:09 +0100

    Update rplidar_s1.launch.py

--- launch/rplidar_s1.launch.py
+++ launch/rplidar_s1.launch.py
@@ -14,7 +14,8 @@ def generate_launch_description():
                 {'serial_baudrate': 256000},
                 {'frame_id': 'laser'},
                 {'inverted': False},
-                {'angle_compensate': True}
+                {'angle_compensate': True},
+                {'scan_mode': 'Sensitivity'}
             ],
         ),
     ])
